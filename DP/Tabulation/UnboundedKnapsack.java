c4e7dac57a44b4c044273b42c11461e3c13b0a3b
Abhay Gupta <58393468+Abhay-G@users.noreply.github.com>
2020-12-09 18:45:00 +0530

    Create UnboundedKnapsack.java

---
+++ DP/Tabulation/UnboundedKnapsack.java
@@ -0,0 +1,32 @@
+class Solution{
+    public static int knapSack(int N, int W, int val[], int wt[])
+    {
+      
+     int t[][] = new int[N+1][W+1];
+     for(int i=0;i<=N;i++)
+     {
+         t[i][0] = 0;
+     }
+      for(int i=0;i<=W;i++)
+     {
+         t[0][i]= 0;
+     }
+     
+     return knap(N,W,val,wt,t);
+    }
+    public static int knap(int n, int W, int val[], int wt[], int[][]t)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=W;j++)
+            {
+                if(wt[i-1]<=j)
+                t[i][j] = Math.max(val[i-1]+t[i][j-wt[i-1]], t[i-1][j]);
+                else
+                t[i][j] = t[i-1][j];
+                
+            }
+        }
+        return t[n][W];
+    }
+}
