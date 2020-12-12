3497877561a169376c1e5d70fb7d0d76a62ee572
Abhay Gupta <58393468+Abhay-G@users.noreply.github.com>
2020-12-12 16:05:36 +0530

    Create MinimumNoOfDeletionToMakeAstringPalidrome.java

---
+++ DP/Tabulation/MinimumNoOfDeletionToMakeAstringPalidrome.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args) {
+	  
+	  
+	  
+	  Scanner sc =  new Scanner(System.in);
+	  int test = sc.nextInt();
+	  while(test-->0)
+	  {
+	      String x = sc.next();
+	      int n = x.length();
+	      int t[][] = new int[n+1][n+1];
+	      for(int i=0;i<n+1;i++)
+	      {
+	          for(int j=0;j<n+1;j++)
+	          {
+	              if(i==0||j==0)
+	              {
+	                  t[i][j] = 0;
+	              }
+	          }
+	      }
+	  
+	  
+	  StringBuilder s = new StringBuilder();
+	  s.append(x);
+	  String y = s.reverse().toString();
+	  
+	  int pal = lps(x,y,n,t);
+	  System.out.println(n-pal);
+		
+		
+	}
+	}
+	public static int lps(String x, String y, int n, int[][]t)
+	{
+	    for(int i=1;i<=n;i++)
+	    {
+	        for(int j=1;j<=n;j++)
+	        {
+	            if(x.charAt(i-1)==y.charAt(j-1))
+	            {
+	                t[i][j] = t[i-1][j-1]+1;
+	            }
+	            else
+	            t[i][j] = Math.max(t[i-1][j], t[i][j-1]);
+	        }
+	    }
+	    return t[n][n];
+	}
+}
