9f711ded16755366e790b821e767ba59d144401f
Abhay Gupta <58393468+Abhay-G@users.noreply.github.com>
2020-12-19 17:49:09 +0530

    Create DuplicateBrackets.java

---
+++ Stack-Queue/DuplicateBrackets.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+       
+       Scanner sc = new Scanner(System.in);
+       String str = sc.nextLine();
+       Stack<Character> st = new Stack<>();
+       for(int i=0;i<str.length();i++)
+       {
+           char ch = str.charAt(i);
+           if(ch==')' && !st.isEmpty())
+           {
+               if(st.peek()=='(')
+               {
+                   System.out.println("true");
+                   return;
+               }
+               else{
+               while(!st.isEmpty()&&st.peek()!='(')
+               {
+                   st.pop();
+               }
+               st.pop();
+               }
+           }
+           else{
+               st.push(ch);
+           }
+       }
+       System.out.println("false");
+    }
