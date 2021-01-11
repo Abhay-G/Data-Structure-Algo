5dc9c8ae4bc3c06c68ad513b4c94fb0ff16cb8c2
Abhay Gupta <58393468+Abhay-G@users.noreply.github.com>
2021-01-11 22:34:55 +0530

    Create construct and display.java

---
+++ Generic-Tree/construct and display.java
@@ -0,0 +1,43 @@
+public class Main {
+    private static class Node {
+        int data;
+        ArrayList < Node > children = new ArrayList < > ();
+    }
+
+    public static void display(Node node) {
+        String str = node.data + " -> ";
+        for (Node child: node.children) {
+            str += child.data + ", ";
+        }
+        str += ".";
+        System.out.println(str);
+
+        for (Node child: node.children) {
+            display(child);
+        }
+    }
+
+    public static Node construct(int[] arr) {
+        Node root = null;
+
+        Stack < Node > st = new Stack < > ();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == -1) {
+                st.pop();
+            } else {
+                Node t = new Node();
+                t.data = arr[i];
+
+                if (st.size() > 0) {
+                    st.peek().children.add(t);
+                } else {
+                    root = t;
+                }
+
+                st.push(t);
+            }
+        }
+
+        return root;
+    }
+    }
